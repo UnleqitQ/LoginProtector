1fb6dd7fbd25af2d4b659b1354ea406f1ff7cc6d
UnleqitQ <quentin.weinbrenner@gmail.com>
2025-03-15 13:16:07 +0100

    add command handling and permissions for LoginProtector

--- src/main/java/com/unleqitq/loginProtector/LoginProtector.java
+++ src/main/java/com/unleqitq/loginProtector/LoginProtector.java
@@ -1,17 +1,32 @@
 package com.unleqitq.loginProtector;
 
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LoginProtector extends JavaPlugin {
+	private ProtectorCommand protectorCommand;
 	
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
 		reloadConfig();
+		registerCommands();
+	}
+	
+	private void registerCommands() {
+		protectorCommand = new ProtectorCommand(this);
+		getServer().getCommandMap().register("loginprotector", protectorCommand);
 	}
 	
 	@Override
 	public void onDisable() {
+		unregisterCommands();
+		protectorCommand = null;
+	}
+	
+	private void unregisterCommands() {
+		SimpleCommandMap commandMap = (SimpleCommandMap) getServer().getCommandMap();
+		commandMap.getKnownCommands().remove("loginprotector");
 	}
 	
 }
---
+++ src/main/java/com/unleqitq/loginProtector/ProtectorCommand.java
@@ -0,0 +1,245 @@
+package com.unleqitq.loginProtector;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginIdentifiableCommand;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ProtectorCommand extends Command implements PluginIdentifiableCommand {
+	
+	private final LoginProtector plugin;
+	
+	public ProtectorCommand(LoginProtector plugin) {
+		super("loginprotector");
+		this.plugin = plugin;
+		setPermission("loginprotector.admin");
+	}
+	
+	@Override
+	public @NotNull Plugin getPlugin() {
+		return plugin;
+	}
+	
+	@Override
+	public boolean execute(
+		@NotNull CommandSender sender, @NotNull String label,
+		String @NotNull [] args
+	) {
+		return switch (args.length == 0 ? "help" : args[0].toLowerCase()) {
+			case "reload" -> {
+				plugin.reloadConfig();
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Config reloaded").color(NamedTextColor.GREEN)));
+				yield true;
+			}
+			case "version" -> {
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Version: " + plugin.getPluginMeta().getVersion())
+						.color(NamedTextColor.GREEN)));
+				yield true;
+			}
+			case "edit" -> onEditCommand(sender, Arrays.copyOfRange(args, 1, args.length));
+			case "help" -> {
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Commands:").color(NamedTextColor.GREEN)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector reload").color(NamedTextColor.GREEN))
+					.append(Component.text(" - Reloads the config").color(NamedTextColor.GRAY)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector version").color(NamedTextColor.GREEN))
+					.append(Component.text(" - Shows the plugin version").color(NamedTextColor.GRAY)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector edit ...").color(NamedTextColor.GREEN))
+					.append(Component.text(" - Edit the config").color(NamedTextColor.GRAY)));
+				yield true;
+			}
+			default -> false;
+		};
+	}
+	
+	private boolean onEditCommand(CommandSender sender, String @NotNull [] args) {
+		return switch (args.length == 0 ? "help" : args[0].toLowerCase()) {
+			case "usernames" -> onEditUsernamesCommand(sender, Arrays.copyOfRange(args, 1, args.length));
+			case "webhook" -> onEditWebhookCommand(sender, Arrays.copyOfRange(args, 1, args.length));
+			case "help" -> {
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Edit config:").color(NamedTextColor.GREEN)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector edit usernames ...").color(NamedTextColor.GREEN))
+					.append(Component.text(" - Edit the usernames list").color(NamedTextColor.GRAY)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector edit webhook <url>").color(NamedTextColor.GREEN))
+					.append(Component.text(" - Edit the webhook").color(NamedTextColor.GRAY)));
+				yield true;
+			}
+			default -> false;
+		};
+	}
+	
+	private boolean onEditUsernamesCommand(CommandSender sender, String @NotNull [] args) {
+		return switch (args.length == 0 ? "help" : args[0].toLowerCase()) {
+			case "add" -> {
+				if (args.length != 2) {
+					sender.sendMessage(Component.text("LoginProtector")
+						.color(NamedTextColor.AQUA)
+						.append(Component.text("» ").color(NamedTextColor.GRAY))
+						.append(Component.text("Missing argument").color(NamedTextColor.RED)));
+					yield false;
+				}
+				List<String> usernames = plugin.getConfig().getStringList("logged-usernames");
+				usernames.add(args[1]);
+				plugin.getConfig().set("logged-usernames", usernames);
+				plugin.saveConfig();
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Username added").color(NamedTextColor.GREEN)));
+				yield true;
+			}
+			case "remove" -> {
+				if (args.length != 2) {
+					sender.sendMessage(Component.text("LoginProtector")
+						.color(NamedTextColor.AQUA)
+						.append(Component.text("» ").color(NamedTextColor.GRAY))
+						.append(Component.text("Missing argument").color(NamedTextColor.RED)));
+					yield false;
+				}
+				try {
+					int index = Integer.parseInt(args[1]);
+					List<String> usernames = plugin.getConfig().getStringList("logged-usernames");
+					if (index < 1 || index > usernames.size()) {
+						sender.sendMessage(Component.text("LoginProtector")
+							.color(NamedTextColor.AQUA)
+							.append(Component.text("» ").color(NamedTextColor.GRAY))
+							.append(Component.text("Invalid index").color(NamedTextColor.RED)));
+						yield false;
+					}
+					usernames.remove(index - 1);
+					plugin.getConfig().set("logged-usernames", usernames);
+					plugin.saveConfig();
+					sender.sendMessage(Component.text("LoginProtector")
+						.color(NamedTextColor.AQUA)
+						.append(Component.text("» ").color(NamedTextColor.GRAY))
+						.append(Component.text("Username removed").color(NamedTextColor.GREEN)));
+					yield true;
+				}
+				catch (NumberFormatException e) {
+					sender.sendMessage(Component.text("LoginProtector")
+						.color(NamedTextColor.AQUA)
+						.append(Component.text("» ").color(NamedTextColor.GRAY))
+						.append(Component.text("Invalid index").color(NamedTextColor.RED)));
+					yield false;
+				}
+			}
+			case "list" -> {
+				List<String> usernames = plugin.getConfig().getStringList("logged-usernames");
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Usernames:")
+						.color(NamedTextColor.GREEN)
+						.append(Component.text(usernames.isEmpty() ? " None" : ""))));
+				for (int i = 0; i < usernames.size(); i++) {
+					sender.sendMessage(Component.text("» ")
+						.color(NamedTextColor.GRAY)
+						.append(Component.text(i + 1 + ": " + usernames.get(i)).color(NamedTextColor.GREEN)));
+				}
+				yield true;
+			}
+			case "help" -> {
+				sender.sendMessage(Component.text("LoginProtector")
+					.color(NamedTextColor.AQUA)
+					.append(Component.text("» ").color(NamedTextColor.GRAY))
+					.append(Component.text("Edit usernames:").color(NamedTextColor.GREEN)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector edit usernames add <username>")
+						.color(NamedTextColor.GREEN))
+					.append(Component.text(" - Add a username").color(NamedTextColor.GRAY)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector edit usernames remove <index>")
+						.color(NamedTextColor.GREEN))
+					.append(Component.text(" - Remove a username").color(NamedTextColor.GRAY)));
+				sender.sendMessage(Component.text("» ")
+					.color(NamedTextColor.GRAY)
+					.append(Component.text("/loginprotector edit usernames list").color(NamedTextColor.GREEN))
+					.append(Component.text(" - List all usernames").color(NamedTextColor.GRAY)));
+				yield true;
+			}
+			default -> false;
+		};
+	}
+	
+	private boolean onEditWebhookCommand(CommandSender sender, String @NotNull [] args) {
+		if (args.length != 1) {
+			sender.sendMessage(Component.text("LoginProtector")
+				.color(NamedTextColor.AQUA)
+				.append(Component.text("» ").color(NamedTextColor.GRAY))
+				.append(Component.text("Missing argument").color(NamedTextColor.RED)));
+			return false;
+		}
+		plugin.getConfig().set("webhook", args[0]);
+		plugin.saveConfig();
+		sender.sendMessage(Component.text("LoginProtector")
+			.color(NamedTextColor.AQUA)
+			.append(Component.text("» ").color(NamedTextColor.GRAY))
+			.append(Component.text("Webhook updated").color(NamedTextColor.GREEN)));
+		return true;
+	}
+	
+	@Override
+	public @NotNull List<String> tabComplete(
+		@NotNull CommandSender sender, @NotNull String label,
+		@NotNull String[] args
+	) {
+		if (args.length == 1) {
+			return Stream.of("reload", "version", "edit", "help")
+				.filter(s -> s.startsWith(args[0].toLowerCase()))
+				.toList();
+		}
+		if (args.length > 1 && args[0].equalsIgnoreCase("edit")) {
+			if (args.length == 2) {
+				return Stream.of("usernames", "webhook", "help")
+					.filter(s -> s.startsWith(args[1].toLowerCase()))
+					.toList();
+			}
+			if (args[1].equalsIgnoreCase("usernames")) {
+				if (args.length == 3) {
+					return Stream.of("add", "remove", "list", "help")
+						.filter(s -> s.startsWith(args[2].toLowerCase()))
+						.toList();
+				}
+				if (args[2].equalsIgnoreCase("remove")) {
+					return plugin.getConfig()
+						.getStringList("usernames")
+						.stream()
+						.map(String::valueOf)
+						.toList();
+				}
+			}
+		}
+		return List.of();
+	}
+	
+}
