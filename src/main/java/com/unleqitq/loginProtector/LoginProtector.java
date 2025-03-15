0b36a4b66e2c25c7f428e15df93d97d2e7450d50
UnleqitQ <quentin.weinbrenner@gmail.com>
2025-03-15 14:14:02 +0100

    add update checker for automatic plugin updates and cleanup of outdated files

--- src/main/java/com/unleqitq/loginProtector/LoginProtector.java
+++ src/main/java/com/unleqitq/loginProtector/LoginProtector.java
@@ -3,25 +3,59 @@ package com.unleqitq.loginProtector;
 import lombok.Getter;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public final class LoginProtector extends JavaPlugin {
+	
+	@Nullable
+	private static LoginProtector instance;
 	
 	@Getter
 	private LoginListener loginListener;
 	private ProtectorCommand protectorCommand;
 	
+	private BukkitTask updateCheckTask;
+	
 	@Override
 	public void onEnable() {
+		instance = this;
+		cleanup();
 		saveDefaultConfig();
 		reloadConfig();
 		configFix();
+		UpdateChecker.scheduleUpdateCheck(task -> updateCheckTask = task);
 		loginListener = new LoginListener(this);
 		loginListener.setup();
 		registerCommands();
+	}
+	
+	public void cleanup() {
+		UpdateChecker.VersionInfo currentVersion =
+			UpdateChecker.VersionInfo.fromString(getPluginMeta().getVersion());
+		for (File file : Objects.requireNonNull(getServer().getPluginsFolder()
+			.listFiles((dir, name) -> name.startsWith("LoginProtector-") && name.endsWith(".jar")))) {
+			String fileName = file.getName();
+			String versionString = fileName.substring(4, fileName.length() - 4);
+			try {
+				UpdateChecker.VersionInfo version = UpdateChecker.VersionInfo.fromString(versionString);
+				if (currentVersion.isNewerThan(version)) {
+					if (!file.delete()) {
+						getLogger().warning("Failed to delete outdated plugin file: " + file.getName());
+					}
+				}
+			}
+			catch (IllegalArgumentException e) {
+				getLogger().warning("Failed to parse version from file name: " + file.getName());
+			}
+		}
 	}
 	
 	public void configFix() {
@@ -68,10 +102,27 @@ public final class LoginProtector extends JavaPlugin {
 	
 	@Override
 	public void onDisable() {
+		if (updateCheckTask != null) {
+			try {
+				updateCheckTask.cancel();
+			}
+			catch (IllegalStateException e) {
+				// Ignore
+			}
+		}
 		loginListener.shutdown();
 		loginListener = null;
 		unregisterCommands();
 		protectorCommand = null;
+		instance = null;
+	}
+	
+	@NotNull
+	public static LoginProtector getInstance() {
+		if (instance == null) {
+			throw new IllegalStateException("Plugin is not enabled");
+		}
+		return instance;
 	}
 	
 	private void unregisterCommands() {
