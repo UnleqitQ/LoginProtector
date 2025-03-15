7841614e5bfe7fa3316890c89922d0b339de8402
UnleqitQ <quentin.weinbrenner@gmail.com>
2025-03-15 13:52:01 +0100

    update config

--- src/main/java/com/unleqitq/loginProtector/LoginProtector.java
+++ src/main/java/com/unleqitq/loginProtector/LoginProtector.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class LoginProtector extends JavaPlugin {
 	
 	@Getter
@@ -14,9 +18,42 @@ public final class LoginProtector extends JavaPlugin {
 	public void onEnable() {
 		saveDefaultConfig();
 		reloadConfig();
+		configFix();
 		loginListener = new LoginListener(this);
 		loginListener.setup();
 		registerCommands();
+	}
+	
+	public void configFix() {
+		boolean save = false;
+		if (!getConfig().contains("webhook", true)) {
+			getConfig().set("webhook", "DISCORD_WEBHOOK_URL");
+			save = true;
+		}
+		if (!getConfig().contains("logged-usernames", true)) {
+			getConfig().set("logged-usernames", new ArrayList<String>());
+			save = true;
+		}
+		if (!getConfig().contains("analysis.interval", true)) {
+			getConfig().set("analysis.interval", 5);
+			getConfig().setInlineComments(
+				"analysis.interval",
+				List.of("seconds between each analysis task run")
+			);
+			save = true;
+		}
+		if (!getConfig().contains("analysis.timeout", true)) {
+			getConfig().set("analysis.timeout", 30);
+			getConfig().setInlineComments(
+				"analysis.timeout",
+				List.of("seconds before a login attempt is considered timed out")
+			);
+			save = true;
+		}
+		
+		if (save) {
+			saveConfig();
+		}
 	}
 	
 	private void registerCommands() {
